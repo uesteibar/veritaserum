b57de061f23863d525d5945fa32e4ab4eb0f9f6b
Lee Chi Ngan <chingan.lee90@gmail.com>
2019-04-24 17:06:17 +0800

    refactor: Switch to Jason as JSON parser

--- lib/evaluator.ex
+++ lib/evaluator.ex
@@ -3,12 +3,10 @@ defmodule Veritaserum.Evaluator do
   Evaluats words, boosters, negators and emoticons.
   """
 
-  alias Poison.Parser
-
   ["word", "emoticon", "negator", "booster"]
   |> Enum.each(fn facet ->
     File.read!("#{__DIR__}/../config/facets/#{facet}.json")
-    |> Parser.parse!()
+    |> Jason.decode!()
     |> (fn list ->
           def unquote(:"#{facet}_list")(),
             do: unquote(list |> Enum.map(fn {key, _} -> key end) |> List.flatten())
